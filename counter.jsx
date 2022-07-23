a7525112abc54312e0c6e3a1fe81bd6b33f13cc8
busari007 <88407563+busari007@users.noreply.github.com>
2022-07-23 22:01:09 +0100

    Update counter.jsx

--- counter.jsx
+++ counter.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 
-class Counter extends Component {
+class Counter extends React.Component {
+    state = {
+        count:0
+    }
+    styles = {
+        fontSize:30,
+        fontWeight:"bold",     //testing styles
+        color:"black"
+    }
     render() { 
-        return <h1>Welcome {this.props.name}</h1>;
+        return (
+        <div>
+        <span style = {this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>  
+        <button className="btn btn-secondary btm-sm m-3">Increment</button>
+        </div>
+        );
+    }
+    formatCount(){
+        const {count} = this.state;                //testing states
+        return count === 0 ? "Zero" : count;
     }
 }
  
